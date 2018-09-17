42985067adcddc5da8cbea5bc442661b151f57cf
EdmondScorp <35503688+EdmondScorp@users.noreply.github.com>
2018-09-17 18:43:13 +0400

    Second Commit
    Improved and nicer code.

--- App/main.js
+++ App/main.js
@@ -1,36 +1,27 @@
-function multiply (a, b) {
-    let result = prompt(`What does ${a} * ${b} equal?`, '');
-    if (+result === a * b) {
-        this.innerText = a * b;
+(() => {
+    function multiply(a, b) {
+        const result = +prompt(`What does ${a} * ${b} equal?`, '');
+        if (result === a * b) 
+            this.innerText = a * b;
+        else 
+            alert('You are so wrong xD');
     }
-    else {
-        alert('You are so wrong xD');
-    }
-}
 
-var table = document.createElement('table');
+    const table = document.createElement('table');
 
-for (let i = 0; i < 10; i++) {
-
-    let tr = document.createElement('tr');
-    table.appendChild(tr);
-
-    for (let j = 0; j < 10; j++) {
-
-        let td = document.createElement('td');
-        td.align = 'center';
-        td.onclick = function() {
-            multiply.call(this, i, j);
-        }
-        if (i === 0 || j === 0){
-            td.innerText = j || i;
-        }
-        tr.appendChild(td);
-
-    }
-}
-
-document.body.appendChild(table);
-
-
-
+    [...Array(10).keys()]
+        .forEach((i, _, a) => {
+            const tr = document.createElement('tr');
+            table.appendChild(tr);
+            a.forEach(j => {
+                const td = document.createElement('td');
+                td.align = 'center';
+                td.onclick = function() {
+                    multiply.call(this, i, j);
+                }
+                if (!i || !j) td.innerText = j || i;
+                tr.appendChild(td);
+            });
+        });
+    document.body.appendChild(table);
+})();
